6c8671b952ded7283c9826eee16174f8b99efcb8
breakbean <breakbean@163.com>
2017-05-18 17:58:51 +0800

    main file

---
+++ main.c
@@ -0,0 +1,28 @@
+nclude <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <config.h>
+#include <encoding_manager.h>
+#include <fonts_manager.h>
+#include <disp_manager.h>
+#include <input_manager.h>
+#include <pic_operation.h>
+#include <render.h>
+#include <string.h>
+#include <picfmt_manager.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+
+/* digitpic <freetype_file> */
+int main(int argc, char **argv)
+{	
+	
+}
